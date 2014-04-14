83b0a713bc7447b3144ecc2dfeb32fa769e2aa66
Harsh Alkutkar <harshalkutkar@gmail.com>
2014-04-14 17:31:32 -0400

    With AlchemyAPi

--- miner.py
+++ miner.py
@@ -4,6 +4,8 @@ import twitter
 import json
 import pprint
 from json import JSONEncoder
+from alchemyapi import AlchemyAPI
+
 import oauthclient
 import functools
 import datetime
@@ -12,8 +14,11 @@ class MyEncoder(JSONEncoder):
     def default(self, o):
         return o.__dict__
 
+#instantiate the alchemy api
+alchemyapi = AlchemyAPI()
 
 
+#instantiate twitter api
 twitapi = twitter.Api(consumer_key='oab3F9Cx5lINWlSadHAL50spo',
                            consumer_secret='DyYPedkhKDgru6H53esW0W2q7e6nGEEYaLA7iTPXLrTOf63hkm',
                            access_token_key='21637668-a2mvcgdOGRE6C5WKA6w6Uim0m4oIfmUxA75oMKkeu',
@@ -27,7 +32,7 @@ result = twitapi.GetSearch(term="\"Arvind Kejriwal\" "
                                 "OR \"Manmohan Singh\" "
                                 "OR \"Rahul Gandhi\" "
                                 "OR \"Sonia Gandhi\"",
-                                count=200)
+                                count=20)
 
 
 print str(result)
@@ -49,6 +54,14 @@ with open('tweets.json', "a") as outfile:
 
             y['time_zone'] = x.user.time_zone
 
+            try:
+                response = alchemyapi.sentiment("text", x.text.encode('utf-8'))
+                sentiment = response["docSentiment"]["score"]
+
+                y['sentiment'] = sentiment
+
+            except KeyError:
+                continue
 
             print "text"+x.text
             print "time"+(datetime.datetime.fromtimestamp(int(x.created_at_in_seconds)).strftime('%Y-%m-%d %H:%M:%S'))
